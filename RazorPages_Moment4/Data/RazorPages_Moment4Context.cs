71e0d657911d66b18f744d8a02b939d5d4496f26
olive <olive@5CG6236643>
2020-11-30 18:18:35 +0100

    Add project files.

---
+++ RazorPages_Moment4/Data/RazorPages_Moment4Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPages_Moment4.Models;
+
+namespace RazorPages_Moment4.Data
+{
+    public class RazorPages_Moment4Context : DbContext
+    {
+        public RazorPages_Moment4Context (DbContextOptions<RazorPages_Moment4Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPages_Moment4.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ RazorPages_Moment4/Models/SeedData.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPages_Moment4.Data;
+using System.Threading.Tasks;
+
+namespace RazorPages_Moment4.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RazorPages_Moment4Context(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<RazorPages_Moment4Context>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "When Harry Met Sally",
+                        ReleaseDate = DateTime.Parse("1989-2-12"),
+                        Genre = "Romantic Comedy",
+                        Price = 7.99M,
+                        Rating = "R"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Ghostbusters ",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Comedy",
+                        Price = 8.99M,
+                        Rating = "G"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Ghostbusters 2",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Genre = "Comedy",
+                        Price = 9.99M,
+                        Rating = "G"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Rio Bravo",
+                        ReleaseDate = DateTime.Parse("1959-4-15"),
+                        Genre = "Western",
+                        Price = 3.99M,
+                        Rating = "NA"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
